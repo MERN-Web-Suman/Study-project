94c32c3980ae5dd893cc81b25f2cf249f6190a4c
suman-coding <sumankumar9798330493@gmail.com>
2024-12-05 17:11:19 +0530

    curd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Curd from './components/Curd';
+function App() {
+  return (
+    <div className="App">
+        <Curd></Curd>
+    </div>
+  );
+}
+
+export default App;
